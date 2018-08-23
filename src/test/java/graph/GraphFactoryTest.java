35f80524eead84a420eed8e32754d294e45fb5cf
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-08-23 13:25:02 +0200

    Refactor test class for GraphFactory

--- src/test/java/graph/GraphFactoryTest.java
+++ src/test/java/graph/GraphFactoryTest.java
@@ -1,7 +1,14 @@
 package graph;
 
+import com.sun.tools.classfile.Opcode;
+import dao.FileWordLoader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,18 +21,18 @@ class GraphFactoryTest {
     private Node testNode2;
     private Node testNode3;
     private Node testNode4;
-    private Node testNode5;
 
     @BeforeEach
     void initializeGraphInstance() {
-        graph = new Graph();
-        graphFactory = new GraphFactory();
-
         testNode1 = new Node("cat");
         testNode2 = new Node("cot");
-        testNode3 = new Node("car");
-        testNode4 = new Node("cog");
-        testNode5 = new Node("dog");
+        testNode3 = new Node("dog");
+        testNode4 = new Node("bug");
+
+        Set set = Mockito.mock(Set.class);
+        String[] test = new String[] {testNode1.getWord().toString(), testNode2.getWord().toString()};
+        graphFactory = new GraphFactory(set, test);
+
     }
 
     @Test
@@ -37,9 +44,8 @@ class GraphFactoryTest {
 
     @Test
     void isAdjacentReturnFalseTest() {
-        boolean isAdjacent = graphFactory.isAdjacent(testNode1.getWord(), testNode5.getWord());
+        boolean isAdjacent = graphFactory.isAdjacent(testNode1.getWord(), testNode4.getWord());
 
         assertTrue(!isAdjacent);
     }
-
 }
