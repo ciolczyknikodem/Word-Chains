d1aef128a59edc0d14cc9cca117c35d5afe17437
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-08-24 13:32:49 +0200

    Add envoirment for test

--- src/test/java/graph/GraphTest.java
+++ src/test/java/graph/GraphTest.java
@@ -3,24 +3,51 @@ package graph;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GraphTest {
 
+    private Map<CharSequence, Node> nodes;
     private Graph graph;
-    private Node node1;
-    private Node node2;
+    private CharSequence startWord;
+    private CharSequence wordBetween1;
+    private CharSequence wordBetween2;
+    private CharSequence endWord;
+
+    private Node startNode;
+    private Node nodeBetween1;
+    private Node nodeBetween2;
+    private Node endNode;
 
     @BeforeEach
-    void initializeGraph() {
-        graph = new Graph();
-        node1 = new Node("cat");
-        node2 = new Node("cot");
+    void initializeGraphAndWordsForTest() {
+        nodes = new HashMap<>();
+        startWord = "cat";
+        wordBetween1 = "cot";
+        wordBetween2 = "cog";
+        endWord = "dog";
+
+        startNode = new Node(startWord);
+        nodeBetween1 = new Node(wordBetween1);
+        nodeBetween2 = new Node(wordBetween2);
+        endNode = new Node(endWord);
+
+        nodes.put(startWord, startNode);
+        nodes.put(wordBetween1, nodeBetween1);
+        nodes.put(wordBetween2, nodeBetween2);
+        nodes.put(endWord, endNode);
+
+        graph = new Graph(nodes);
     }
 
     @Test
     void addNodeTest() {
-        graph.addNode(node1);
+        graph.addNode(new Node(wordBetween2));
 
         Node result = graph.getNode("cat");
         String expected = "cat";
