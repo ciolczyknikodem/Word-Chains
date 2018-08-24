cdcc60cf2b6c2af73483270090c517d1b9e67717
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-08-24 15:02:19 +0200

    Add test if factory adding nodes to graph

--- src/test/java/graph/GraphFactoryTest.java
+++ src/test/java/graph/GraphFactoryTest.java
@@ -1,10 +1,13 @@
 package graph;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mockito;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,7 +25,7 @@ class GraphFactoryTest {
     private Node testNode2;
     private Node testNode3;
     private Node testNode4;
-    
+
 
     @BeforeEach
     void initializeEnvironmentForTest() {
@@ -30,20 +33,21 @@ class GraphFactoryTest {
         word2 = "cot";
         word3 = "dog";
         word4 = "bug";
-        
+
         testNode1 = new Node(word1);
         testNode2 = new Node(word2);
         testNode3 = new Node(word3);
         testNode4 = new Node(word4);
-        
+
         wordsContainer = new HashSet<>();
-        
+
         wordsContainer.add(word1);
         wordsContainer.add(word2);
         wordsContainer.add(word3);
         wordsContainer.add(word4);
 
         graphFactory = new GraphFactory(wordsContainer);
+        graphFactory.handleBuildGraph();
 
     }
 
@@ -59,5 +63,19 @@ class GraphFactoryTest {
         boolean isAdjacent = graphFactory.isAdjacent(testNode1.getWord(), testNode4.getWord());
 
         assertFalse(isAdjacent);
+    }
+
+    @Test
+    void isGraphFactoryAddNodeCorrectlyTest() {
+        Map<CharSequence, Node> nodes = graphFactory.getGraph().getGraph();
+
+        assertTrue(nodes.containsKey(word1));
+    }
+
+    @Test
+    void lookForWordNotExistInGraphTest() {
+        Map<CharSequence, Node> nodes = graphFactory.getGraph().getGraph();
+
+        assertFalse(nodes.containsKey("wrongKey"));
     }
 }
