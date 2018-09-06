a97d9005b53e6cafffa788f3c51fcb25cdaf1a4d
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-09-06 14:21:49 +0200

    Add handler for redirect to result page and call graph algorithm

--- src/main/java/controller/WordChainController.java
+++ src/main/java/controller/WordChainController.java
@@ -2,6 +2,8 @@ package controller;
 
 import model.ChainPath;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,4 +28,17 @@ public class WordChainController {
         );
         return "result";
     }
+
+    @ModelAttribute
+    public void addAttribute(Model model) {
+        model.addAttribute("chainPath", chainPath);
+    }
+
+    @RequestMapping(value = "/result", method = RequestMethod.POST)
+    public String resultPostView(@ModelAttribute ChainPath chainPath) {
+        WordChain wordChain = new WordChain();
+        wordChain.handleSearchWordChain(chainPath);
+        return "result";
+    }
+
 }
