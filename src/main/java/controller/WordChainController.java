dcc165ac5100229f927e67b13444e7a9dedc2ec3
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-09-06 14:19:23 +0200

    Add request handler for main page

--- src/main/java/controller/WordChainController.java
+++ src/main/java/controller/WordChainController.java
@@ -2,10 +2,18 @@ package controller;
 
 import model.ChainPath;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class WordChainController {
 
     private ChainPath chainPath = new ChainPath();
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String indexView() {
+        return "index";
+    }
 
 }
