81424e43f94cc6c80b14f78c88f3233c1e3f561a
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-09-06 14:20:21 +0200

    Add request post method handler for main page

--- src/main/java/controller/WordChainController.java
+++ src/main/java/controller/WordChainController.java
@@ -4,7 +4,7 @@ import model.ChainPath;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WordChainController {
@@ -16,4 +16,14 @@ public class WordChainController {
         return "index";
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public String findChainPath(@RequestParam("startWord") CharSequence startWord,
+                                @RequestParam("endWord") CharSequence endWord) {
+
+        chainPath = new ChainPath(
+                startWord,
+                endWord
+        );
+        return "result";
+    }
 }
