caa7a410cc04a5ac136106fa0bc0a5d7334112b1
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-09-06 14:16:57 +0200

    Add controller class for request handlers, Spring

---
+++ src/main/java/controller/WordChainController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import model.ChainPath;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WordChainController {
+
+    private ChainPath chainPath = new ChainPath();
+
+}
